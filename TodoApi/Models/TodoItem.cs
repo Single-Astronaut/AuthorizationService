ff22b9f37d2b03ff47e5643b91bc28d17370e819
Nikita Shendelov <shendelov_na@bars-arenda.ru>
2024-12-25 15:26:12 +0400

    first

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; } //Свойство Id выступает в качестве уникального ключа реляционной базы данных
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; } //Предотвращение избыточной публикации с применением DTO, добавляем поле секрета
+}
